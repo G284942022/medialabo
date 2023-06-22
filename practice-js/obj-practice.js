0f919c53a37e4d1876bd2ffb99823689fe2d89dd
G284942022 <228494@st.takushoku-u.ac.jp>
2023-06-22 15:24:45 +0900

    gg

---
+++ practice-js/obj-practice.js
@@ -0,0 +1,15 @@
+let media = {
+    adress: "八王子市館町",
+    room: ["D101", "D102", "D103", "D201", "D202", "D203", "D204", "D205"],
+    department: ["機械システム工学科", "電子システム工学科", "情報工学科", "デザイン学科"],
+}
+
+console.log(media.adress);
+
+for (let i=0; i<ns.length; i=i+1) {
+    console.log(media.room[i]);
+}
+
+for (let i=0; i<ns.length; i=i+1) {
+    console.log(media.department[i]);
+}
