4353d7c9b6e120f28725546eecfaf2f78e4d04bf
G284942022 <228494@st.takushoku-u.ac.jp>
2023-06-29 16:25:39 +0900

    melte

--- practice-dom/dom-exercise.js
+++ practice-dom/dom-exercise.js
@@ -15,12 +15,51 @@ let data = [
 
 // 練習4-2 メッセージ追加プログラム
 
+let newElement = document.createElement("p");
+newElement.style = "text-emphasis: sesame green;";
+newElement.textContent = "写真表と都市の緯度経度のページです";
+
+let heading = document.querySelector("h2#ex42");
+heading.insertAdjacentElement("afterend", newElement);
 
 // 練習4-3 写真表作成プログラム
+
+let p1 = document.createElement("p");
+let img1 = document.createElement("img");
+img1.src = "taro.png";
+p1.appendChild(img1);
+
+let p2 = document.createElement("p");
+let img2 = document.createElement("img");
+img2.src = "jiro.png";
+p2.appendChild(img2);
+
+let p3 = document.createElement("p");
+let img3 = document.createElement("img");
+img3.src = "hanako.png";
+p3.appendChild(img3);
+
+let phototable = document.getElementById("phototable");
+phototable.appendChild(p1);
+phototable.appendChild(p2);
+phototable.appendChild(p3);
+
 
 
 // 練習4-4 箇条書き削除プログラム
 
+let list = document.getElementById("location");
+list.innerHTML = "";
 
 // 練習4-5 箇条書き追加プログラム
+
+data.forEach(city => {
+  let listItem = document.createElement("li");
+  listItem.textContent = `${city.name}...緯度:${city.lat}, 経度:${city.lng}`;
+  list.appendChild(listItem);
+});
+
+
+
+
 
